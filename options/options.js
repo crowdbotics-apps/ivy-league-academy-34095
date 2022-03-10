f69a98fccdc040a1cad7d62b537901819a63ad62
Crowdbotics <team@crowdbotics.com>
2022-03-10 04:48:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "ivy_league_academy_34095",
+  url: "https://ivy_league_academy_34095.botics.co",
+  api: "https://ivy_league_academy_34095.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
